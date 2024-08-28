90cb442e978d61aae57df435349c4f18e82cbbd8
doug <doug.bercot@gmail.com>
2024-08-28 13:17:35 -0400

    Authentication

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,8 +13,23 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://mongodb:27017/mean-tutorial?authSource=admin',
-    { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoURI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
+
+mongoose.connect(mongoURI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // auth: {
+        //     user: MONGODB_USER,
+        //     password: MONGODB_PASSWORD
+        // },
+        // authSource: 'admin'
+    })
+    .then(() => console.log('Connected to mongodb'))
+    .catch((err) => {
+        console.error(`Error connecting to mongodb at \"${mongoURI}\"`, err)
+        process.exit(1);  // Exit the process with an error code (1 indicates an error)
+    });
 
 const connection = mongoose.connection;
 connection.once('open', () => {
