8d010c7b37683b1cab58291a01b8c68d2c8a942d
Doug <doug.bercot@gmail.com>
2024-08-28 22:52:12 -0400

    Running but weirdness

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,20 @@
 //server.js
 
 import {Connection} from 'mongoose';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
 
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-// const bodyParser = require('body-parser');
-require('dotenv').config();
+const bodyParser = require('body-parser');
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-// app.use(bodyParser.json({}));
+app.use(bodyParser.json({}));
 
 // MongoDB connection
 const mongoURI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
